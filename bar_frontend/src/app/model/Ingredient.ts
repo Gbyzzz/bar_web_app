3e27cb53314e2bfa8940a21cb2b262a8846607c6
Anton Pinchuk <gbyzzz@gmail.com>
2022-05-26 17:43:41 +0400

    added ingredient add

--- bar_frontend/src/app/model/Ingredient.ts
+++ bar_frontend/src/app/model/Ingredient.ts
@@ -5,7 +5,7 @@ export class Ingredient {
   unitOfMeasurement: string;
 
 
-  constructor(ingredientId: number, ingredientName: string, ingredientAlcohol: number, unitOfMeasurement: string) {
+  constructor(ingredientName: string, ingredientAlcohol: number, unitOfMeasurement: string, ingredientId?: number) {
     this.ingredientId = ingredientId;
     this.ingredientName = ingredientName;
     this.ingredientAlcohol = ingredientAlcohol;
--- bar_frontend/src/app/view/add-cocktail/add-cocktail.component.ts
+++ bar_frontend/src/app/view/add-cocktail/add-cocktail.component.ts
@@ -3,6 +3,10 @@ import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angul
 import {HttpClient} from "@angular/common/http";
 import {Ingredient} from "../../model/Ingredient";
 import {IngredientServiceImpl} from "../../service/impl/IngredientServiceImpl";
+import {EditIngredientDialogComponent} from "../dialog/edit-ingredient-dialog/edit-ingredient-dialog.component";
+import {DialogAction} from "../dialog/DialogResult";
+import {MatDialog} from "@angular/material/dialog";
+import * as punycode from "punycode";
 
 @Component({
   selector: 'app-add-cocktail',
@@ -15,10 +19,12 @@ export class AddCocktailComponent implements OnInit {
   selectedIngredient: Ingredient[] = [];
   selectedUnit: string[] = [];
   cocktailForm: FormGroup;
+  newIngredient: Ingredient;
 
   ingredients: Ingredient[];
 
   constructor(private httpClient: HttpClient,
+              private dialog: MatDialog,
               private ingredientService: IngredientServiceImpl,
               private fb: FormBuilder) {
 
@@ -31,8 +37,6 @@ export class AddCocktailComponent implements OnInit {
 
     ingredientService.findAll().subscribe(ingredients =>{
       this.ingredients = ingredients;
-      this.selectedIngredient.splice(0, 0, this.ingredients[0]);
-      this.selectedUnit.splice(0, 0, this.selectedIngredient[0].unitOfMeasurement);
     });
   }
 
@@ -48,6 +52,9 @@ export class AddCocktailComponent implements OnInit {
   }
 
   addIngredientsRecipe() {
+    this.selectedIngredient.splice(this.selectedIngredient.length, 0, this.ingredients[0]);
+    this.selectedUnit.splice(this.selectedUnit.length,0, this.ingredients[0].unitOfMeasurement);
+    console.log(this.selectedIngredient);
 
     this.ingredientsRecipe().push(this.newIngredientsRecipe());
   }
@@ -58,9 +65,9 @@ export class AddCocktailComponent implements OnInit {
 
   onSelectChange(event, i){
     console.log(event);    console.log(i);
-
-    this.selectedIngredient.splice(i, i, event);
-    this.selectedUnit.splice(i, i, this.selectedIngredient[i].unitOfMeasurement);
+    this.selectedIngredient[i] = event;
+    this.selectedUnit[i] = this.selectedIngredient[i].unitOfMeasurement;
+    console.log(this.selectedUnit);
   }
 
   onFileChange(event) {
@@ -76,6 +83,32 @@ export class AddCocktailComponent implements OnInit {
 
       };
     }
+  }
+  addIngredient(ingredient: Ingredient){
+    this.ingredientService.add(ingredient).subscribe();
+  }
+
+  openAddDialog(): void {
+    this.newIngredient = new Ingredient('',0,'');
+
+    const dialogRef = this.dialog.open(EditIngredientDialogComponent, {
+      data: [this.newIngredient],
+      autoFocus: false
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+
+      if (!(result)) {
+        return;
+      }
+
+      if (result.action === DialogAction.SAVE) {
+        console.log(this.newIngredient);
+        this.addIngredient(this.newIngredient);
+
+        return;
+      }
+    });
   }
 
   ngOnInit(): void {
