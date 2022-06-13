e6ba2512f9d1a14e45e5fe64914b798c42c6ed06
Anton Pinchuk <gbyzzz@gmail.com>
2022-06-14 01:13:47 +0400

    added user pagination

--- bar_spring/src/main/java/com/gbyzzz/bar_spring/exception/ControllerAdvice.java
+++ bar_spring/src/main/java/com/gbyzzz/bar_spring/exception/ControllerAdvice.java
@@ -1,6 +1,5 @@
 package com.gbyzzz.bar_spring.exception;
 
-import com.gbyzzz.bar_spring.entity.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
