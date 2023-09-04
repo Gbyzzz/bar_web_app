b5e9503757f20cd9a0c6db1bbdc7bad618ec3fc6
Anton Pinchuk <gbyzzz@gmail.com>
2023-09-04 11:18:47 +0300

    Some bugfixes

--- bar_backend/src/main/java/db/migration/V2__Images_Adding.java
+++ bar_backend/src/main/java/db/migration/V2__Images_Adding.java
@@ -5,6 +5,7 @@ import org.flywaydb.core.api.migration.Context;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,40 +18,33 @@ public class V2__Images_Adding extends BaseJavaMigration {
 
 
     public void migrate(Context context) {
-        FileInputStream fileInputStream = null;
-        try (PreparedStatement statement = context.getConnection().prepareStatement("INSERT INTO images (name, filename, content_type, size, bytes) VALUES (?, ?, ?, ?, ?);")) {
-            for(int i = FIRST_IMAGE_NAME; i<=LAST_IMAGE_NAME; i++){
+        try (PreparedStatement statement = context.getConnection()
+                .prepareStatement("INSERT INTO images (name, filename, content_type, size, bytes)" +
+                        " VALUES (?, ?, ?, ?, ?);")) {
+            for (int i = FIRST_IMAGE_NAME; i <= LAST_IMAGE_NAME; i++) {
                 String filename = i + ".jpg";
-                fileInputStream = new FileInputStream(V2__Images_Adding.class.getClassLoader().getResource("images").getPath() +"/" + filename);
-                byte[] arr = fileInputStream.readAllBytes();
-                int size = fileInputStream.available();
-                statement.setString(1, "file");
-                statement.setString(2, filename);
-                statement.setString(3, "image/jpeg");
-                statement.setInt(4, size);
-                statement.setBytes(5, arr);
-                statement.executeUpdate();
 
+                InputStream inputStream = V2__Images_Adding.class.getClassLoader().getResourceAsStream("images/" + filename);
 
+                if (inputStream != null) {
+                    byte[] arr = inputStream.readAllBytes();
+                    int size = arr.length;
+
+                    statement.setString(1, "file");
+                    statement.setString(2, filename);
+                    statement.setString(3, "image/jpeg");
+                    statement.setInt(4, size);
+                    statement.setBytes(5, arr);
+                    statement.executeUpdate();
+
+                    inputStream.close();
+                } else {
+                    // Handle the case where the resource was not found
+                    System.err.println("Resource not found: images/" + filename);
+                }
             }
-//            try (ResultSet rows = statement.executeQuery("SELECT id FROM person ORDER BY id")) {
-//                while (rows.next()) {
-//                    int id = rows.getInt(1);
-//                    String anonymizedName = "Anonymous" + id;
-//                    try (Statement update = context.getConnection().createStatement()) {
-//                        update.execute("UPDATE person SET name='" + anonymizedName + "' WHERE id=" + id);
-//                    }
-//                }
-//            }
         } catch (SQLException | IOException e) {
-        e.printStackTrace();
-    } finally {
-        // Close the resources
-        try {
-            if (fileInputStream != null) fileInputStream.close();
-        } catch (IOException e) {
             e.printStackTrace();
         }
-    }
     }
 }
