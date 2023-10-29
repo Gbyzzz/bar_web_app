d0bd792905949beb419f5cb1f65ea7b039fe2b83
Anton Pinchuk <gbyzzz@gmail.com>
2023-10-29 12:34:14 +0200

    dependency fix

--- bar_frontend/src/app/service/auth/auth.service.ts
+++ bar_frontend/src/app/service/auth/auth.service.ts
@@ -1,19 +1,20 @@
-  import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-  import {PasswordChange} from "../../model/registration/PasswordChange";
-  import {environment} from "../../../environments/environment";
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {PasswordChange} from "../../model/registration/PasswordChange";
+import {environment} from "../../../environments/environment";
 
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   login(username: string, password: string): Observable<any> {
     return this.http.post(environment.API_URL + 'sign_in', {
@@ -33,6 +34,7 @@ export class AuthService {
   logout(): Observable<any> {
     return this.http.post(environment.API_URL + 'sign_out', httpOptions);
   }
+
   checkPassword(passwordChange: PasswordChange): Observable<any> {
     return this.http.post<any>(environment.API_URL + 'check_password', passwordChange);
   }
