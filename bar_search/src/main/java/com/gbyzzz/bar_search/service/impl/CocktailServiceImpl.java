a038264a6db0ca03ed480fe906c5f62a85aa8efe
Anton Pinchuk <gbyzzz@gmail.com>
2024-10-12 23:30:58 +0300

    some updates

--- bar_search/src/main/java/com/gbyzzz/bar_search/service/impl/CocktailServiceImpl.java
+++ bar_search/src/main/java/com/gbyzzz/bar_search/service/impl/CocktailServiceImpl.java
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
